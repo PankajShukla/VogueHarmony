933d2df2892839ae57d03af8fc5ebb12539511b3
Pankaj Shukla <pankaj.shukla3821@gmail.com>
2023-10-17 21:03:53 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,4 +1,11 @@
-pip install streamlit-image-select
+
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+install('streamlit-image-select')
 
 import streamlit as st
 import streamlit.components.v1 as components
